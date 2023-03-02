34dddb64e1e73d7c7c6123cb15101baee0cdb27b
Pavel Drozhzhachih <pavel.drozhzhachih@gmail.com>
2023-03-02 21:42:06 +0300

    fix: deleted unused annotations, add ObjectSupplier

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserUpdateDto.java
@@ -2,7 +2,6 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.AccessLevel;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.validation.constraints.Email;
